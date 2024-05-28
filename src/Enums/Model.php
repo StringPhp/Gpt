750cf84d61b25ae2b3a34d7879e13264ece0ff05
Robert Snedeker <rsnedeker20@gmail.com>
2024-05-28 13:11:37 -0400

    added missing enum

--- src/Enums/Model.php
+++ src/Enums/Model.php
@@ -7,4 +7,5 @@ enum Model: string
     case GPT_4o = 'gpt-4o';
     case GPT_4o_2024_05_13 = 'gpt-4o-2024-05-13';
     case GPT_4_TURBO = 'gpt-4-turbo';
+    case GPT_4_TURBO_2024_04_09 = 'gpt-4-turbo-2024-04-09';
 }
