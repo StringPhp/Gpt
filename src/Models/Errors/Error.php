152fdaeb683148318ad1fdac2147bf44f9ae2618
Robert Snedeker <rsnedeker20@gmail.com>
2024-06-18 14:47:41 -0400

    Added missing type

--- src/Models/Errors/Error.php
+++ src/Models/Errors/Error.php
@@ -2,6 +2,7 @@
 
 namespace StringPhp\Gpt\Models\Errors;
 
+use StringPhp\Models\DataTypes\NullType;
 use StringPhp\Models\DataTypes\StringType;
 use StringPhp\Models\JsonModel;
 
@@ -11,6 +12,7 @@ class Error extends JsonModel
     public string $type;
 
     #[StringType(false)]
+    #[NullType(false)]
     public ?string $param;
 
     #[StringType(false)]
